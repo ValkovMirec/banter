3e607b706f52823d6184bd7318d10bf8add994f4
Miroslav Valkovič <miroslav.valkovic@bootiq.io>
2023-07-14 16:56:35 +0200

    fixed condition for sign in button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,48 +18,47 @@ export default function Home() {
         <div className="px-2 py-1 mr-1">
           <UserButton afterSignOutUrl="/"/>
         </div>
-
-        {!!user.isSignedIn ??
-            <div className="rounded-sm m-1 px-2 py-1 text-white bg-emerald-900">
-              <SignInButton />
-            </div>
-        }
-
-
       </header>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+        {!!user.isSignedIn ??
+          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+              Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            </h1>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+              <Link
+                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                href="https://create.t3.gg/en/usage/first-steps"
+                target="_blank"
+              >
+                <h3 className="text-2xl font-bold">First Steps →</h3>
+                <div className="text-lg">
+                  Just the basics - Everything you need to know to set up your
+                  database and authentication.
+                </div>
+              </Link>
+              <Link
+                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                href="https://create.t3.gg/en/introduction"
+                target="_blank"
+              >
+                <h3 className="text-2xl font-bold">Documentation →</h3>
+                <div className="text-lg">
+                  Learn more about Create T3 App, the libraries it uses, and how
+                  to deploy it.
+                </div>
+              </Link>
+            </div>
+            <p className="text-2xl text-white">
+              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            </p>
           </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
+        }
+        {!user.isSignedIn ??
+          <div className="rounded-sm m-1 px-2 py-1 text-white bg-emerald-900">
+            <SignInButton />
+          </div>
+        }
       </main>
     </>
   );
