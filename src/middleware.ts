980413b515fe4f263ec2ae99995532f236ed59bd
Miroslav Valkovič <miroslav.valkovic@bootiq.io>
2023-07-14 16:28:55 +0200

    clerk auth foundation added

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
+
+export default authMiddleware({
+    debug: true,
+    publicRoutes: ["/"],
+    beforeAuth: (req) => {
+        console.log('>>> LIFT OFF <<<')
+    },
+    clockSkewInSeconds: 50,
+});
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
