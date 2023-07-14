580c8af19e2760aab927208b48965baaf796e6ce
Miroslav Valkovič <miroslav.valkovic@bootiq.io>
2023-07-14 16:57:25 +0200

    added public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 
 export default authMiddleware({
+    publicRoutes: ["/"],
     beforeAuth: (req) => {
         console.log('>>> LIFT OFF <<<')
     },
